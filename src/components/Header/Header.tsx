431b8fb737aa0104303560ffd80cfb041bfe5eaa
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-16 07:35:02 +0000

    Update header section
    - added some style changes
    - added link placeholders for now , need to change them to links or buttons that take to diff sections on the landing page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const TopSection = () => {
   return (
-    <div className="flex justify-center items-center border border-b-gray-100 h-24">
-      <div className="p-4 rounded-full border border-stone-100">
+    <div className="flex justify-center items-start mb-6">
+      <div className="p-4 w-4/5 rounded-full border border-stone-100">
         {/* logo and links in a container */}
         <div className="flex justify-between">
           {/* logo side */}
@@ -13,11 +13,16 @@ export const TopSection = () => {
               alt="logo"
               className="size-5 rounded shrink-0"
             />
-            <span className="text-sm font-bold text-orange-400">Website</span>
+            <span className="text-sm font-extrabold">Website</span>
+          </div>
+          {/* links */}
+          <div className="text-xs flex gap-6 items-center">
+            <span>Home</span>
+            <span>Features</span>
+            <span>Update</span>
+            <span>Pricing</span>
           </div>
         </div>
-
-        {/* links */}
 
         {/* sign-up button */}
       </div>
