c16e2ff30c58fa0facfa468ab755aef269d4c010
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-16 09:26:04 +0000

    Update style for header section of landing page
    - fixed some style issues
    - need more refinement later

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,28 +3,29 @@ import React from "react";
 export const TopSection = () => {
   return (
     <div className="flex justify-center items-start mb-6">
-      <div className="p-4 w-4/5 rounded-full border border-stone-100">
-        {/* logo and links in a container */}
-        <div className="flex justify-between">
-          {/* logo side */}
-          <div className="flex gap-1.5 items-center">
-            <img
-              src="https://api.dicebear.com/9.x/identicon/svg?seed=Maria"
-              alt="logo"
-              className="size-5 rounded shrink-0"
-            />
-            <span className="text-sm font-extrabold">Website</span>
-          </div>
-          {/* links */}
+      <div className="flex p-4 w-3/5 rounded-full border border-stone-100 justify-between">
+        {/* logo */}
+        <div className="flex gap-1.5 items-center">
+          <img
+            src="https://api.dicebear.com/9.x/identicon/svg?seed=Maria"
+            alt="logo"
+            className="size-5 rounded shrink-0"
+          />
+          <span className="text-sm font-extrabold">Website</span>
+        </div>
+        <div className="flex gap-10">
+          {/* links + button*/}
           <div className="text-xs flex gap-6 items-center">
             <span>Home</span>
             <span>Features</span>
             <span>Update</span>
             <span>Pricing</span>
           </div>
-        </div>
 
-        {/* sign-up button */}
+          <button className="text-xs px-3 py-1.5 border rounded-full border-stone-400">
+            Log in
+          </button>
+        </div>
       </div>
     </div>
   );
