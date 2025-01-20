255842007711536f8da566652c37cfa84ca671a3
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-20 07:35:50 +0000

    Update style for landing page
    - change height of page to 100vh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { TopSection } from "@/components/TopSection/TopSection";
 
 export default function Home() {
   return (
-    <main className="h-[800vh]">
+    <main className="h-[100vh]">
       {/* link components here */}
       <TopSection />
     </main>
