0843cf5c250f9d9bc4eccbfeecab063f750e68f1
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-25 09:24:34 +0000

    Add hero section below header
    - styled it a bit , changed the font style and centred it all
    - put all that in a container with top rounded corners

--- src/components/TopSection/Hero.tsx
+++ src/components/TopSection/Hero.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="m-4 pt-24 border bg-orange-500 rounded-t-2xl">
+      <div className="flex justify-center px-[256px] text-6xl text-center font-serif text-white">
+        Collaborate smarter with streamlined workflow
+      </div>
+    </div>
+  );
 };
