fd62fe6379f5c15467ca7ec89c37922e70413782
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-28 07:05:20 +0000

    Add features section
    - basic imports handled
    - no style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Features } from "@/components/Features/Features";
 import { TopSection } from "@/components/TopSection/TopSection";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="h-[100vh]">
       {/* link components here */}
       <TopSection />
+      <Features />
     </main>
   );
 }
