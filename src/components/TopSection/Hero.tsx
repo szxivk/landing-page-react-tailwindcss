cd6099775557f820b9e5f4edf77aaa59881307f7
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-28 06:59:26 +0000

    Update styles for hero section
    - removed bg-color and border
    - adjusted gaps and element spacings

--- src/components/TopSection/Hero.tsx
+++ src/components/TopSection/Hero.tsx
@@ -3,17 +3,17 @@ import { FaArrowRight } from "react-icons/fa";
 
 export const Hero = () => {
   return (
-    <div className="mx-6 mt-4 pt-36 border bg-orange-200 rounded-t-2xl flex-col pl-[400px] pr-[256px] pb-2">
+    <div className="mx-6 mt-4 pt-24 flex-col pl-[400px] pr-[256px] pb-2">
       <h1 className="text-[90px] leading-none font-bold text-right font-serif text-orange-500">
         Collaborate smarter with streamlined workflow
       </h1>
-      <div className="flex justify-end pt-2 gap-2 items-end">
+      <div className="flex justify-end pt-2 gap-4 items-end">
         <input
           type="text"
           placeholder="Enter your email"
           className="bg-orange-300 rounded gap-1 px-2 py-1 text-sm w-auto placeholder:text-[#2b4149]/40 focus:outline-none"
         />
-        <button className="p-1 text-sm flex gap-1 items-center shadow bg-stone-50 rounded">
+        <button className="p-1 text-sm flex gap-1 items-center shadow bg-stone-50 rounded mr-5">
           Get Demo
           <FaArrowRight />
         </button>
