f9860966e33d6d6f214f92f3efced0a58a31ce47
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-28 09:44:00 +0000

    Update features section layout and style
    - added a basic bento setup
    - styled it a bit
    - ignore the section border for now

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+"use client";
+import { motion, MotionProps } from "framer-motion";
+import { twMerge } from "tailwind-merge";
 
 export const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="mx-52 mt-2 grid h-auto grid-cols-12 gap-4 rounded border border-orange-700 p-4">
+      <Block className="col-span-6"></Block>
+      <Block />
+      <Block />
+      <Block />
+      <Block />
+      <Block />
+    </div>
+  );
+};
+
+type Props = {
+  className?: string;
+} & MotionProps;
+
+const Block = ({ className, ...rest }: Props) => {
+  return (
+    <motion.div
+      className={twMerge(
+        "col-span-4 rounded-lg border border-orange-200 bg-orange-400 p-6 text-[#f2f7fa]",
+        className,
+      )}
+      {...rest}
+    />
+  );
 };
