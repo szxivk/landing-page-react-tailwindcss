66374f0f3c2f4c9524b653523698da1641a6bf40
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-29 07:16:55 +0000

    Update style and layout of the features section
    - put up a desired basic bento layout

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -4,13 +4,11 @@ import { twMerge } from "tailwind-merge";
 
 export const Features = () => {
   return (
-    <div className="mx-52 mt-2 grid h-auto grid-cols-12 gap-4 rounded border border-orange-700 p-4">
-      <Block className="col-span-6"></Block>
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
+    <div className="mx-52 mt-14 grid h-auto grid-flow-dense grid-cols-9 gap-2 p-4">
+      <HeaderBlock />
+      <Productivity />
+      <MoreFeatures />
+      <Shortcuts />
     </div>
   );
 };
@@ -23,10 +21,26 @@ const Block = ({ className, ...rest }: Props) => {
   return (
     <motion.div
       className={twMerge(
-        "col-span-4 rounded-lg border border-orange-200 bg-orange-400 p-6 text-[#f2f7fa]",
+        "col-span-4 rounded-xl border border-orange-200 bg-orange-400 p-6 text-[#f2f7fa]",
         className,
       )}
       {...rest}
     />
   );
+};
+
+const HeaderBlock = () => {
+  return <Block className="md:col-span-3 md:row-span-10"></Block>;
+};
+
+const Productivity = () => {
+  return <Block className="md:col-span-2 md:row-span-10"></Block>;
+};
+
+const MoreFeatures = () => {
+  return <Block className="md:col-span-4 md:row-span-12"></Block>;
+};
+
+const Shortcuts = () => {
+  return <Block className="md:col-span-5 md:row-span-2"></Block>;
 };
