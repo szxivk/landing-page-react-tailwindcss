6a5f2fe9c0dcf399002edebda9aa9c35a09d66d4
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-31 16:17:44 +0000

    Update styles and layout for footer section
    - tailwind css is not fun when we have to copy paste same style again and again but I think my approach was wrong rather -there should be a better way
    
    - cheated here a bit -used chatgpt to convert hover ani I saw on a website to tailwind -in my defense I was a bit tired -wont do it again promise

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,8 +9,8 @@ import {
 
 export const Footer = () => {
   return (
-    <div className="mt-24 w-full rounded-3xl border-t border-gray-500 px-44 py-24">
-      <div className="flex items-center justify-between">
+    <div className="mt-24 w-full bg-white py-24 pl-52">
+      <div className="flex items-start justify-between">
         <div className="">
           <div className="flex items-center gap-2">
             <img
@@ -48,6 +48,106 @@ export const Footer = () => {
           </div>
         </div>
         {/* all links in row of 3 */}
+        <div className="flex gap-40 text-sm">
+          <ul>
+            <li className="mb-6 font-semibold">Product</li>
+            <li className="mb-3">
+              <a
+                href="#"
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Admissions
+              </a>
+            </li>
+            <li className="mb-3">
+              <a
+                href="#
+              "
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Charting
+              </a>
+            </li>
+            <li className="mb-3">
+              <a
+                href="#"
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Billing
+              </a>
+            </li>
+            <li className="mb-3">
+              <a
+                href="#"
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Scheduling
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <li className="mb-6 font-semibold">Company</li>
+            <li className="mb-3">
+              <a
+                href="#"
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Features
+              </a>
+            </li>
+            <li className="mb-3">
+              <a
+                href="#"
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Why Website?
+              </a>
+            </li>
+            <li className="mb-3">
+              <a
+                href="#"
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Blog
+              </a>
+            </li>
+            <li className="mb-3">
+              <a
+                href="#"
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Testimonials
+              </a>
+            </li>
+          </ul>
+          <ul className="mr-72">
+            <li className="mb-6 font-semibold">Support</li>
+            <li className="mb-3">
+              <a
+                href="#"
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Contact Us
+              </a>
+            </li>
+            <li className="mb-3">
+              <a
+                href="#"
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Privacy Policy
+              </a>
+            </li>
+            <li className="mb-3">
+              <a
+                href="#"
+                className="relative before:absolute before:bottom-0 before:left-0 before:h-[1px] before:w-full before:origin-right before:scale-x-0 before:transform before:rounded-md before:bg-orange-500 before:transition-transform before:duration-300 before:ease-in-out hover:before:origin-left hover:before:scale-x-100"
+              >
+                Terms of Service
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
