f36d5a7b0b938e63773c045386ee442a1fd8b7df
szxivk <50396748+szxivk@users.noreply.github.com>
2025-02-03 11:16:45 +0000

    Update header style
    - changed its bg and converted route spans to links

--- src/components/TopSection/Header.tsx
+++ src/components/TopSection/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = () => {
   return (
     <div className="flex h-auto justify-center">
-      <div className="flex w-3/5 justify-between border-b border-[#2b4149]/10 pb-3 pt-2">
+      <div className="flex w-3/5 justify-between rounded-full border border-gray-200 bg-white pb-3 pt-2 text-[#1E212B] shadow">
         {/* logo */}
         <div className="flex items-center gap-1.5">
           <img
@@ -16,10 +16,10 @@ export const Header = () => {
         <div className="flex gap-16">
           {/* links + button*/}
           <div className="flex items-center gap-6 text-sm">
-            <span>Home</span>
-            <span>Features</span>
-            <span>Update</span>
-            <span>Pricing</span>
+            <a href="#">Home</a>
+            <a href="#">Features</a>
+            <a href="#">Update</a>
+            <a href="#">Pricing</a>
           </div>
 
           <button className="mr-5 flex shrink-0 items-center justify-center rounded-full border-b-2 border-orange-700 bg-orange-500 px-6 py-2.5 text-sm text-[#f2f7fa] shadow-lg shadow-orange-600/60 hover:bg-orange-600">
